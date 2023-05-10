39ee32b4347066ce36f541dc936c55690473fb07
Vincent Arne L. Vargas <vincentarnel.vargas@Vincents-MacBook-Air.local>
2023-05-10 09:06:16 +0800

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+// GLOBAL - NO WINDOW!!!
+
+//__dirname - path to currenrt directory
+//__filename - file name
+// require - function to use modules (CommonJS)
+// module - infro about current module (file)
+// process - info about env where program is being executed
+
+console.log(__dirname);
+setInterval(() => {
+  console.log("hello world");
+}, 1000);
---
+++ 4-names.js
@@ -0,0 +1,8 @@
+const secret = "SUPER SECRET";
+const john = "John";
+const peter = "Peter";
+
+module.exports = {
+  peter,
+  john,
+};
